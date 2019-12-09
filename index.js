e064cd70092ceec2b3f5d1c1dbf93389927a6a23
Dustin Wells <dustinwells@dustins-imac.hi.hubbell-ad.com>
2019-12-09 17:12:52 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.post('/api/user', (req,res) => {
+    // Need to handle the req data and store to mongoose.
+    res.send(200);
+});
+
+app.get('/api/user', (req,res) => {
+    res.send(200);
+})
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
